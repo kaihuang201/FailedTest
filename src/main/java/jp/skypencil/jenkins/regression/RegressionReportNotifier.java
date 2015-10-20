240bf1dedb2df43ac8bea5d5b0040cc4ee20c915
Felicia Chandra <fchandr2@illinois.edu>
2015-10-20 14:49:42 -0500

    Added a new page to project actions.

--- src/main/java/jp/skypencil/jenkins/regression/RegressionReportNotifier.java
+++ src/main/java/jp/skypencil/jenkins/regression/RegressionReportNotifier.java
@@ -7,8 +7,10 @@ import hudson.Launcher;
 import hudson.Util;
 import hudson.model.BuildListener;
 import hudson.model.Result;
+import hudson.model.TransientProjectActionFactory;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
+import hudson.model.Action;
 import hudson.model.User;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
@@ -20,6 +22,8 @@ import hudson.tasks.test.AbstractTestResultAction;
 import hudson.tasks.test.TestResult;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -274,5 +278,24 @@ public final class RegressionReportNotifier extends Notifier {
         public String getDisplayName() {
             return Messages.RegressionReportNotifier_DisplayName();
         }
+    }
+    
+    @Extension
+    public static final class TestResultsAnalyzerExtension extends TransientProjectActionFactory{
+
+    	@Override
+    	public Collection<? extends Action> createFor(@SuppressWarnings("rawtypes") AbstractProject target) {
+    		
+    		final List<RegressionReportAction> projectActions = target
+                    .getActions(RegressionReportAction.class);
+            final ArrayList<Action> actions = new ArrayList<Action>();
+            if (projectActions.isEmpty()) {
+                final RegressionReportAction newAction = new RegressionReportAction(target);
+                actions.add(newAction);
+                return actions;
+            } else {
+                return projectActions;
+            }
+    	}
     }
 }
