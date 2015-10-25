c4f9e22174c289dfe03e9ce9ec208f6b81857edb
Felicia Chandra <fchandr2@illinois.edu>
2015-10-25 18:18:29 -0500

    fchandr2_hzhang49 Added a new page which listed all build numbers and timestamp.

--- src/main/java/jp/skypencil/jenkins/regression/RegressionReportAction.java
+++ src/main/java/jp/skypencil/jenkins/regression/RegressionReportAction.java
@@ -1,11 +1,19 @@
 package jp.skypencil.jenkins.regression;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.Actionable;
+import hudson.model.Run;
+import hudson.tasks.junit.PackageResult;
+import hudson.tasks.junit.TestResult;
+import hudson.tasks.test.AbstractTestResultAction;
+import hudson.util.RunList;
+import net.sf.json.JSONObject;
 
 public class RegressionReportAction extends Actionable implements Action {
 @SuppressWarnings("rawtypes") AbstractProject project;
@@ -55,4 +63,18 @@ public class RegressionReportAction extends Actionable implements Action {
     	return this.project;
     }
     
+    public List<JSONObject> getBuilds() {
+    	List<JSONObject> builds = new ArrayList<JSONObject>();
+		RunList<Run> runs = project.getBuilds();
+		Iterator<Run> runIterator = runs.iterator();
+		while (runIterator.hasNext()) {
+			Run run = runIterator.next();
+			JSONObject build = new JSONObject();
+			build.put("buildNumber", run.getNumber());
+			build.put("timestamp", run.getTimestampString2());
+			builds.add(build);
+		}
+		
+		return builds;
+    }
 }
