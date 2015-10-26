9deff1735818195f3abca7bce8cbd6ea31286b5f
Felicia Chandra <fchandr2@illinois.edu>
2015-10-26 00:16:56 -0500

    fchandr2_hzhang49 Updated build list page to use a real java object instead of json. Made the table nicer and sortable.

--- src/main/java/jp/skypencil/jenkins/regression/RegressionReportAction.java
+++ src/main/java/jp/skypencil/jenkins/regression/RegressionReportAction.java
@@ -1,22 +1,23 @@
 package jp.skypencil.jenkins.regression;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import hudson.ExtensionPoint;
 import hudson.model.AbstractProject;
 import hudson.model.Action;
 import hudson.model.Actionable;
 import hudson.model.Run;
-import hudson.tasks.junit.PackageResult;
-import hudson.tasks.junit.TestResult;
-import hudson.tasks.test.AbstractTestResultAction;
 import hudson.util.RunList;
-import net.sf.json.JSONObject;
 
 public class RegressionReportAction extends Actionable implements Action {
-@SuppressWarnings("rawtypes") AbstractProject project;
+	@SuppressWarnings("rawtypes")
+	AbstractProject project;
 	
 	public RegressionReportAction(@SuppressWarnings("rawtypes") AbstractProject project){
 		this.project = project;
@@ -62,19 +63,44 @@ public class RegressionReportAction extends Actionable implements Action {
 	public AbstractProject getProject(){
     	return this.project;
     }
-    
-    public List<JSONObject> getBuilds() {
-    	List<JSONObject> builds = new ArrayList<JSONObject>();
+
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public List<Build> getBuilds() {
+		List<Build> builds = new ArrayList<Build>();
 		RunList<Run> runs = project.getBuilds();
 		Iterator<Run> runIterator = runs.iterator();
 		while (runIterator.hasNext()) {
 			Run run = runIterator.next();
-			JSONObject build = new JSONObject();
-			build.put("buildNumber", run.getNumber());
-			build.put("timestamp", run.getTimestampString2());
+			Build build = new Build(run.getNumber(), run.getTimestamp(), run.getTimestampString2());
 			builds.add(build);
 		}
 		
 		return builds;
-    }
+	}
+
+	public static class Build implements ExtensionPoint {
+		private int number;
+		private Calendar timestamp;
+		private String timestampString;
+
+		@DataBoundConstructor
+		public Build(int number, Calendar timestamp, String timestampString) {
+			this.number = number;
+			this.timestamp = timestamp;
+			this.timestampString = timestampString;
+		}
+
+		public int getNumber() {
+			return number;
+		}
+
+		public String getTimestampString() {
+			return timestampString;
+		}
+
+		public String getReadableTimestamp() {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy h:mm:ss a");
+			return dateFormat.format(timestamp.getTime());
+		}
+	}
 }
