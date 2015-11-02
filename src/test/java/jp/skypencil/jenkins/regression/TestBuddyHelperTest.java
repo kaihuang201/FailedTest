5be3ceed98421f76ecf2b744d95135b75650121a
Kai Huang <khuang29@illinois.edu>
2015-11-01 18:54:41 -0600

    khuang29 Added tests for getAllCaseResultsForBuild.

--- src/test/java/jp/skypencil/jenkins/regression/TestBuddyHelperTest.java
+++ src/test/java/jp/skypencil/jenkins/regression/TestBuddyHelperTest.java
@@ -14,8 +14,12 @@ import org.jvnet.hudson.test.JenkinsRule;
 import hudson.maven.MavenModuleSet;
 import jp.skypencil.jenkins.regression.TestBuddyAction.BuildInfo;
 import jp.skypencil.jenkins.regression.TestBuddyAction.TestInfo;
+import jp.skypencil.jenkins.regression.TestBuddyHelper;
 
-public class TestBuddyActionTest {
+import hudson.model.AbstractBuild;
+import hudson.tasks.junit.CaseResult;
+
+public class TestBuddyHelperTest {
 	@Rule
 	public JenkinsRule j = new JenkinsRule();
 	private MavenModuleSet project;
@@ -42,11 +46,17 @@ public class TestBuddyActionTest {
 	}
 	
 	@Test
-	public void testGetAllCaseResultForBuild1() {
+	public void testGetAllCaseResultsForBuild1() {
+        AbstractBuild build = project.getBuildByNumber(1);
+        List<CaseResult> case_results = TestBuddyHelper.getAllCaseResultsForBuild(build);
+        assertEquals(case_results.size(), 3);
 	}
 
 	@Test
-	public void testGetAllCaseResultForBuild2() {
+	public void testGetAllCaseResultsForBuild2() {
+        AbstractBuild build = project.getBuildByNumber(4);
+        List<CaseResult> case_results = TestBuddyHelper.getAllCaseResultsForBuild(build);
+        assertEquals(case_results.size(), 4);
 	}
 
     @Test
