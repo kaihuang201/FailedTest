df89e4788d203066e997eeaf8ac150c4b50d5d26
sunjw413 <sunjw413@gmail.com>
2015-11-02 20:57:51 -0600

    yjong2_marones2 Update js code for user story 2

--- src/main/webapp/js/test-buddy.js
+++ src/main/webapp/js/test-buddy.js
@@ -1,18 +1,8 @@
 function toggleReg() {
-	if (document.getElementById('Regression').checked === false) {
-		jQuery('#mytable tbody tr td:contains("Newly Failed")').parent().toggle();
-	}
-	else if (document.getElementById('Regression').checked === true){
-		jQuery('#mytable tbody tr td:contains("Newly Failed")').parent().toggle();
-	}
+	jQuery('#mytable tbody tr td:contains("Newly Failed")').parent().toggle();
 }
 function toggleProg(){
-	if (document.getElementById('Progression').checked === false) {
-		jQuery('#mytable tbody tr td:contains("Newly Passed")').parent().toggle();	
-	}
-	else if (document.getElementById('Progression').checked === true) {
-		jQuery('#mytable tbody tr td:contains("Newly Passed")').parent().toggle();
-	}
+	jQuery('#mytable tbody tr td:contains("Newly Passed")').parent().toggle();
 }
 
 clearFilters = function(tableID) {
