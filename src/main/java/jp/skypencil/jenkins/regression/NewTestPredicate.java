cf628a4cb7532da938bc5d3a5035bc1f2dd443b7
Kai Huang <kaihuang201@users.noreply.github.com>
2015-12-07 23:29:45 -0600

    Comment NewTestPredicate

--- src/main/java/jp/skypencil/jenkins/regression/NewTestPredicate.java
+++ src/main/java/jp/skypencil/jenkins/regression/NewTestPredicate.java
@@ -4,6 +4,9 @@ import com.google.common.base.Predicate;
 
 import hudson.tasks.junit.CaseResult;
 
+/**
+ * Filter for filtering out Pairs that has a null second element.
+ */
 class NewTestPredicate implements Predicate<Pair<CaseResult, CaseResult>> {
 	@Override
 	public boolean apply(Pair<CaseResult, CaseResult> input) {
